a0fa91ec6714f188a1eca5057e3b70c57225fe94
Axel Fernández <axl1210@estudiantec.cr>
2018-11-03 23:33:33 -0600

    quitar búsqueda

--- inicio.php
+++ inicio.php
@@ -27,9 +27,7 @@
   <body id="page-top">
 
     <nav class="navbar navbar-expand navbar-dark bg-dark static-top">
-
       <a class="navbar-brand mr-1" href="inicio.php">Inicio</a>
-
       <button class="btn btn-link btn-sm text-white order-1 order-sm-0" id="sidebarToggle" href="#">
         <i class="fas fa-bars"></i>
       </button>
@@ -37,10 +35,7 @@
       <!-- Navbar Search -->
       <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-3 my-2 my-md-0">
         <div class="input-group">
-          <input type="text" class="form-control" placeholder="Buscar..." aria-label="Search" aria-describedby="basic-addon2">
           <div class="input-group-append">
-            <button class="btn btn-primary" type="button">
-              <i class="fas fa-search"></i>
             </button>
           </div>
         </div>
